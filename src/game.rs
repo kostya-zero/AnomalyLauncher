8e547a54d48b06dc9883e588a56cc6cc25caf178
Konstantin Zhigaylo <zero@kostyazero.com>
2025-10-12 16:30:10 +0300

    fix(game): wrong executable selection

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::{Result, anyhow};
 use std::{env, process::Command};
 
 use crate::Renderer;
@@ -6,14 +6,14 @@ use crate::Renderer;
 pub fn launch_game(dx_level: Renderer, use_avx: bool, args: Vec<String>) -> Result<()> {
     let mut cwd = env::current_dir().unwrap();
     match (dx_level, use_avx) {
-        (Renderer::DX8, true) => cwd.push("bin\\AnomalyDX8.exe"),
-        (Renderer::DX8, false) => cwd.push("bin\\AnomalyDX8AVX.exe"),
-        (Renderer::DX9, true) => cwd.push("bin\\AnomalyDX9.exe"),
-        (Renderer::DX9, false) => cwd.push("bin\\AnomalyDX9AVX.exe"),
-        (Renderer::DX10, true) => cwd.push("bin\\AnomalyDX10.exe"),
-        (Renderer::DX10, false) => cwd.push("bin\\AnomalyDX10AVX.exe"),
-        (Renderer::DX11, true) => cwd.push("bin\\AnomalyDX11.exe"),
-        (Renderer::DX11, false) => cwd.push("bin\\AnomalyDX11AVX.exe"),
+        (Renderer::DX8, false) => cwd.push("bin\\AnomalyDX8.exe"),
+        (Renderer::DX8, true) => cwd.push("bin\\AnomalyDX8AVX.exe"),
+        (Renderer::DX9, false) => cwd.push("bin\\AnomalyDX9.exe"),
+        (Renderer::DX9, true) => cwd.push("bin\\AnomalyDX9AVX.exe"),
+        (Renderer::DX10, false) => cwd.push("bin\\AnomalyDX10.exe"),
+        (Renderer::DX10, true) => cwd.push("bin\\AnomalyDX10AVX.exe"),
+        (Renderer::DX11, false) => cwd.push("bin\\AnomalyDX11.exe"),
+        (Renderer::DX11, true) => cwd.push("bin\\AnomalyDX11AVX.exe"),
     };
     if !cwd.exists() {
         return Err(anyhow!("Executable not found"));
